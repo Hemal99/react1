413a3a0bff01925d95c7b3decf74bd500f17475d
Hemal <h.githmin@gmail.com>
2020-10-26 21:13:05 +0530

    Single page application part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg ,CardText, CardBody,CardTitle} from 'reactstrap';
+import {Card, CardImg ,CardText, CardBody,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     // eslint-disable-next-line no-useless-constructor
@@ -56,18 +57,33 @@ import {Card, CardImg ,CardText, CardBody,CardTitle} from 'reactstrap';
     const DishDetail=(props)=>{
         const dish=props.dish;
         if(dish!=null){
-            return(
+            return (
                 <div className="container">
-                    <div className="row">
-                        <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.dish.comments}/>
-                    </div>
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
                 </div>
+                <div className="Container">
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} />
+                    </div>
+                    
+                </div>
+                </div>
+        
             );
         }
         else{
             return(
-                <div></div>
+                <div></div> 
             );
         }
     }
